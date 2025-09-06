eccad7b334169edfec9e852827cb9110fae7ef02
nicoQuilpatay <143046619+nicoQuilpatay@users.noreply.github.com>
2025-09-06 14:45:47 -0300

    Se quitaron datos en mongo

--- index.js
+++ index.js
@@ -47,8 +47,6 @@ app.use("/api/firma", firmaRoutes);
 
 // Conexión a MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
 })
 .then(() => {
   console.log("✅ Conectado a MongoDB Atlas");
