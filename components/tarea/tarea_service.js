0e02fadadfcbdf91b1ca556048708e1a376786e9
nicoQuilpatay <143046619+nicoQuilpatay@users.noreply.github.com>
2025-09-11 00:01:24 -0300

    Correccion de commits y creacion de patch tarea

--- components/tarea/tarea_service.js
+++ components/tarea/tarea_service.js
@@ -36,5 +36,25 @@ export const TareaService = {
 
   async eliminarTarea(id) {
     return await TareaModel.findByIdAndDelete(id);
-  }
+  },
+  async actualizarTarea(id, data) {
+  // Excluir campos que no deben ser actualizados
+  const { _id, createdAt, updatedAt, ...datosActualizables } = data;
+  
+  return await TareaModel.findByIdAndUpdate(
+    id,
+    { 
+      ...datosActualizables,
+      ultima_modificacion: new Date() // Actualizar la fecha de última modificación
+    },
+    { 
+      new: true, // Retornar el documento actualizado
+      runValidators: true // Ejecutar validaciones del schema
+    }
+  )
+  .populate("id_area", "nombre_area")
+  .populate("id_sector", "nombre_sector")
+  .populate("id_empresa", "nombre_empresa")
+  .populate("responsable", "nombre_usuario email");
+}
 };
